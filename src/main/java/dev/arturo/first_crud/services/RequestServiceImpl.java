25698341fa354fa60b65ba33529d7d39ace663ae
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-07-24 15:18:13 +0200

    make my RequestServiceImpl

--- src/main/java/dev/arturo/first_crud/services/RequestServiceImpl.java
+++ src/main/java/dev/arturo/first_crud/services/RequestServiceImpl.java
@@ -4,45 +4,67 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import dev.arturo.first_crud.Repository.RequestRepository;
 import dev.arturo.first_crud.models.Request;
 
 
 @Service
 public class RequestServiceImpl implements RequestService {
 
+    private final RequestRepository requestRepository;
+
+    @Autowired
+    public RequestServiceImpl(RequestRepository requestRepository){
+        this.requestRepository = requestRepository;
+    }
+
     @Override
     public List<Request> findAllRequests() {
-        throw new UnsupportedOperationException("Unimplemented method 'findAllRequests'");
+       return requestRepository.findAll();
     }
 
     @Override
     public Optional<Request> findRequestById(Long id) {
-        throw new UnsupportedOperationException("Unimplemented method 'findRequestById'");
+        if (id == null) {
+            throw new IllegalArgumentException("Id cant be null!");
+        }
+
+        return requestRepository.findById(id);
     }
 
     @Override
     public List<Request> findRequestBySolicitantName(String solicitantName) {
-        throw new UnsupportedOperationException("Unimplemented method 'findRequestBySolicitantName'");
+
+        if (solicitantName.trim().isEmpty() || solicitantName == null) {
+            throw new IllegalArgumentException("Name cant be empty, must set a name");
+        }
+
+        return requestRepository.findBySolicitantName(solicitantName);
     }
 
     @Override
     public List<Request> findRequestByDate(LocalDateTime requestDate) {
-        throw new UnsupportedOperationException("Unimplemented method 'findRequestByDate'");
+        if (requestDate == null) {
+            throw new IllegalArgumentException("Request Date cant be null");
+        }
+    return requestRepository.findByRequestDate(requestDate);
     }
 
     @Override
     public List<Request> findRequestBySubjectConsult(String subjectConsult) {
-        throw new UnsupportedOperationException("Unimplemented method 'findRequestBySubjectConsult'");
+        if (subjectConsult.trim().isEmpty()) {
+        throw new IllegalArgumentException("The consult, must has a subject!");    
+        }
+
+        return requestRepository.findBySubjectConsult(subjectConsult);
     }
 
     @Override
     public List<Request> findRequestByDescriptionConsult(String descriptionConsult) {
-        throw new UnsupportedOperationException("Unimplemented method 'findRequestByDescriptionConsult'");
+    return requestRepository.findByDescriptionConsult(descriptionConsult);
+
     }
-
-
-
-    
 }
