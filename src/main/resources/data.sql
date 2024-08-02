68ef476e75f13f2d1f638e533d2cd4660a63a829
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-08-02 17:08:07 +0200

    adding changes in the data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO users (id, username, password) VALUES (1, 'user1', 'password1');
-INSERT INTO users (id, username, password) VALUES (2, 'user2', 'password2');
+INSERT INTO users (username, password) VALUES ('user1', 'password1');
+INSERT INTO users (username, password) VALUES ('user2', 'password2');
 
-INSERT INTO requests (id, solicitant_name, request_date, subject_consult, description_consult) VALUES 
-(1, 'Juan Perez', CURRENT_TIMESTAMP, 'Consulta 1', 'Descripción de la consulta 1'),
-(2, 'Maria Lopez', CURRENT_TIMESTAMP, 'Consulta 2', 'Descripción de la consulta 2'),
-(3, 'Carlos Diaz', CURRENT_TIMESTAMP, 'Consulta 3', 'Descripción de la consulta 3');
+INSERT INTO requests (solicitant_name, request_date, subject_consult, description_consult) VALUES 
+('Bumby', CURRENT_TIMESTAMP, 'Consulta 1', 'Descripción de la consulta 1'),
+('Lucas', CURRENT_TIMESTAMP, 'Consulta 2', 'Descripción de la consulta 2'),
+('Pupi', CURRENT_TIMESTAMP, 'Consulta 3', 'Descripción de la consulta 3');
